59d65d09ea6fcaf56cfdd1bdca6981cac2652e78
Keineik <duckienvanba@gmail.com>
2025-03-16 23:48:41 +0700

    change output format of OverlapDetection to requested format

--- src/spark/OverlapDetection.py
+++ src/spark/OverlapDetection.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import array_sort, asc
+from pyspark.sql.functions import array_sort, asc, expr
 
 # Get spark session
 spark = SparkSession.builder\
@@ -21,7 +21,10 @@ df = df.alias("df1").crossJoin(df.alias("df2"))\
             df1.vertices[1][1] > df2.vertices[2][1] AND\
             df1.vertices[2][0] > df2.vertices[1][0] AND\
             df1.vertices[2][1] < df2.vertices[1][1]")\
-    .orderBy(asc("df1.shape_id"), asc("df2.shape_id"))
-
+    .withColumn("shape_1", expr("CAST(SUBSTRING(df1.shape_id, 7, LENGTH(df1.shape_id)-1) as INT)"))\
+    .withColumn("shape_2", expr("CAST(SUBSTRING(df2.shape_id, 7, LENGTH(df2.shape_id)-1) as INT)"))\
+    .orderBy(asc("shape_1"), asc("shape_2"))\
+    .select("shape_1", "shape_2")\
+    
 df.show(15, truncate=False)
 df.selectExpr("count(1) as cnt").show()
